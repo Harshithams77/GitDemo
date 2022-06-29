3c28f19937da3efa1f1a486b48aad4f343dad5c3
harsh <harshitha.murali15@gmail.com>
2022-06-29 15:11:33 +0530

    first commit

---
+++ src/test/java/qaclickacademy/RESTApiTest.java
@@ -0,0 +1,16 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class RESTApiTest {
+	@Test
+	public void postJIRA()
+	{
+		System.out.println("postJIRA");
+	}
+	@Test
+	public void deleteTwitter()
+	{
+		System.out.println("deleteTwitter");
+	}
+}
